93438782b1490fe22a436efce73b6026cbe5b269
amarten <alexdmarten@gmail.com>
2021-12-08 13:57:03 -0800

    store city searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,21 @@ var excludeAlerts = 'Alerts';
 var cityLat;
 var cityLong;
 var cityAPISearch = [];
-var citySearchString = 'Oakland';
+var citySearchString;
+var searchHistory = [];
+const searchButtonEl = $('#search');
+const cityInputEl = $('#city-search');
 
+
+var retrieveHistory = function() {
+    storedHistory = JSON.parse(localStorage.getItem('citySearchHistory'));
+
+    if (storedHistory) {
+        searchHistory = storedHistory;
+    }
+}
+
+retrieveHistory();
 
 var testWeatherAPI = function(cityLat, cityLong) {
     currentWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLong}&units=${'imperial'}&exclude=${excludeAlerts}&appid=${'94e32ddc97880c45b19a69dfc85aec8d'}`;
@@ -25,13 +38,26 @@ var testCityAPI = function(city) {
             console.log(cityLat, cityLong)
             testWeatherAPI(cityLat, cityLong);
         })
-        // .then(data => cityLat = data.data[0].latitude)
 
 };
 
 var userCitySearch = function() {
+    citySearchString = cityInputEl[0].value;
+    console.log(citySearchString)
     
+    if(citySearchString) {
+        // testCityAPI(citySearchString);
+        searchHistory.push(citySearchString);
+        localStorage.setItem('citySearchHistory', JSON.stringify(searchHistory));
+        cityInputEl[0].value = "";
+    }
+    else {
+        alert("Please enter a city.")
+    }
 }
+
 
 // testCityAPI(citySearchString);
 // https://api.openweathermap.org/data/2.5/onecall?lat=38.7521&lon=121.2880&exclude=Alerts&appid=APIKey
+
+searchButtonEl.on('click', userCitySearch)
